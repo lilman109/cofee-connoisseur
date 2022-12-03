c04853259cdce3a1051ebffc397fe158bd4b9c86
lilman109 <akira.yoshidasjsu@gmail.com>
2022-12-03 21:19:18 +0900

    get coffee stores from api

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner/Banner';
 import Card from '../components/Card/Card';
 import styles from '../styles/Home.module.css';
 import coffeeStores from '../data/coffee-stores.json';
+import axios from 'axios';
 
 export default function Home({ coffeeStores }) {
 	const handleOnBannerBtnClick = () => {
@@ -32,11 +33,14 @@ export default function Home({ coffeeStores }) {
 							{coffeeStores.map((store, idx) => {
 								return (
 									<Card
-										key={idx}
+										key={store.fsq_id}
 										className={styles.card}
 										name={store.name}
-										href={`/coffee-store/${store.id}`}
-										imgUrl={store.imgUrl}
+										href={`/coffee-store/${store.fsq_id}`}
+										imgUrl={
+											store.imgUrl ||
+											'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+										}
 									/>
 								);
 							})}
@@ -49,9 +53,20 @@ export default function Home({ coffeeStores }) {
 }
 
 export const getStaticProps = async (context) => {
+	const res = await axios.get(
+		'https://api.foursquare.com/v3/places/search?query=coffee&ll=35.57%2C139.56&limit=6',
+		{
+			headers: {
+				Authorization: process.env.FOURSQUARE_API_KEY,
+			},
+		}
+	);
+
+	const data = res.data;
+
 	return {
 		props: {
-			coffeeStores,
+			coffeeStores: data.results,
 		},
 	};
 };
