92cb0b1c5960f7a7c392755b0fc4a1d6fbdcda36
lilman109 <akira.yoshidasjsu@gmail.com>
2022-12-03 16:50:16 +0900

    styled dynamic pages

--- pages/coffee-store/[id]/index.js
+++ pages/coffee-store/[id]/index.js
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import coffeestores from '../../../data/coffee-stores.json';
 import Head from 'next/head';
+import styles from './CoffeeStore.module.css';
+import Image from 'next/image';
+import cls from 'classnames';
 
 export const CoffeStore = ({ coffeeStore }) => {
 	const router = useRouter();
@@ -12,16 +15,47 @@ export const CoffeStore = ({ coffeeStore }) => {
 		return <div>Loading....</div>;
 	}
 
-	const { address, name, neighbourhood } = coffeeStore;
+	const handleUpvoteButton = () => {
+		console.log('handle upvote');
+	};
+
+	const { address, name, neighbourhood, imgUrl } = coffeeStore;
 	return (
-		<div>
+		<div className={styles.layout}>
 			<Head>
 				<title>{name}</title>
 			</Head>
-			<Link href={'/'}>Back to Home</Link>
-			<p>{address}</p>
-			<p>{name}</p>
-			<p>{neighbourhood}</p>
+
+			<div className={styles.container}>
+				<div className={styles.col1}>
+					<div className={styles.backToHomeLink}>
+						<Link href={'/'}>Back to Home</Link>
+					</div>
+					<div className={styles.nameWrapper}>
+						<p className={styles.name}>{name}</p>
+					</div>
+					<Image src={imgUrl} width={600} height={360} alt={name} />
+				</div>
+
+				<div className={cls('glass', styles.col2)}>
+					<div className={styles.iconWwrapper}>
+						<Image src='/static/icons/places.svg' width={'24'} height={'24'} />
+						<p className={styles.text}>{address}</p>
+					</div>
+					<div className={styles.iconWwrapper}>
+						<Image src='/static/icons/nearMe.svg' width={'24'} height={'24'} />
+						<p className={styles.text}>{neighbourhood}</p>
+					</div>
+					<div className={styles.iconWwrapper}>
+						<Image src='/static/icons/star.svg' width={'24'} height={'24'} />
+						<p className={styles.text}>{1}</p>
+					</div>
+
+					<button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+						Up Vote!
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
